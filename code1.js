15c2ef10091370bfcfee8fac6b3f1b8e934f08cb
terema001 <terema1999@gmail.com>
2023-11-10 17:02:09 +0200

    Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.

--- code1.js
+++ code1.js
@@ -1,1 +1,12 @@
-let b = c
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function num(a) {
+    if (a % 2 == 0) {
+    console.log("the number is even")
+} 
+else {
+    console.log("the number is odd")
+}
+}
+
+num(6)
